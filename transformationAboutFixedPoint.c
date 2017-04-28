2dbe4729af5d17da648243ba8853fc5c9405e3ed
Edwin Clement <edwinclement08@gmail.com>
2017-04-28 17:52:37 +0530

    updated with fixed point rotation

--- transformationAboutFixedPoint.c
+++ transformationAboutFixedPoint.c
@@ -63,30 +63,66 @@ void midPointCircle(float cx, float cy, float radius)	{
 	}
 }
 
-
-void setIndentity()	{
+void setIndentity(float matrix[3][3])	{
 	int i,j;
 	for(i = 0;i < 3; i++)
 		for(j = 0; j< 3; j++)
-			if(i == j) transformMatrix[i][j] = 1;
-			else transformMatrix[i][j] = 0;
+			if(i == j) matrix[i][j] = 1;
+			else matrix[i][j] = 0;
 }
 
 void setRotation(float angle) {
 	float c = cos(3.141 / 180 * angle);
 	float s = sin(3.141 / 180 * angle);
 
-	setIndentity();
+	setIndentity(transformMatrix);
 	transformMatrix[0][0] = c; transformMatrix[0][1] = -s;
 	transformMatrix[1][0] = s; transformMatrix[1][1] = c;
 }
 
-void setTranslation(float x, float y) {
-	setIndentity();
+void setTranslate(float x, float y) {
+	setIndentity(transformMatrix);
 	transformMatrix[0][2] = x;
 	transformMatrix[1][2] = y;
 }
 
+void multiplyMatrix(float mat1[3][3], float mat2[3][3])	{
+	// gives mat1 * mat2 in mat1
+	float output[3][3] = {0};
+
+	int i, j, k;
+	for(i = 0; i < 3; i++)
+		for(j = 0; j < 3; j++)
+			for(k = 0; k < 3; k++)
+				output[i][j] += mat1[i][k] * mat2[k][j];
+
+	// copy back to mat1
+	for(i = 0; i < 3; i++)
+		for(j = 0; j < 3; j++)
+			mat1[i][j] = output[i][j];
+}
+
+void doRotation(float angle) {
+	float c = cos(3.141 / 180 * angle);
+	float s = sin(3.141 / 180 * angle);
+
+	float output[3][3];
+	setIndentity(output);
+	output[0][0] = c; output[0][1] = -s;
+	output[1][0] = s; output[1][1] = c;
+
+	multiplyMatrix(transformMatrix, output);
+}
+
+
+void doTranslate(float x, float y) {
+	float output[3][3];
+	setIndentity(output);
+
+	output[0][2] = x;
+	output[1][2] = y;
+	multiplyMatrix(transformMatrix, output);
+}
 
 // the function definition should be of type :  void fn_name(void)
 void display()	{
@@ -96,18 +132,40 @@ void display()	{
 	glColor3f(0,0,0);						// set r,g,b to 0,0,0 i.e. black
 											// 3f means 3 floats
 
-	midPointCircle(100,100, 50);
+//	midPointCircle(100,100, 50);
 
 	// put tx = 200
-	setTranslation(200, 0);
-	midPointCircle(100,100, 50);
+//	setTranslate(200, 0);
+//	midPointCircle(100,100, 50);
 
 	// setRotation as 10, 20, 30, 40 w.r.t origin
+//	int i;
+//	for(i = 10; i < 50; i+= 10)	{
+//		setRotation(i);
+//		midPointCircle(100,100, 50);
+//	}
+
+//	doRotation(10);
+//	midPointCircle(100,100, 50);
+//	doRotation(10);
+//	midPointCircle(100,100, 50);
+
 	int i;
-	for(i = 10; i < 50; i+= 10)	{
-		setRotation(i);
-		midPointCircle(100,100, 50);
+	for( i = 10; i < 180; i+=10){
+
+		setTranslate(250, 250);
+		midPointCircle(0,0, 50);		// now thats is reference circle
+		//assuming rotation about 100, 100, 40 deg
+		doTranslate(-100, -100);
+		doRotation(i);
+		doTranslate(100, 100);
+
+		midPointCircle(0,0, 50);		// now thats is reference circle
 	}
+
+
+
+
 
 	glFlush();								// send all the buffer data to screen
 
@@ -121,7 +179,7 @@ int main(int argc, char** argv) {
 											// Create a window with the specified title
 
 	// The only *glu* function here in main
-	gluOrtho2D(-200,500, -200, 500);				// define a 2D orthographic projection matrix
+	gluOrtho2D(0,500, 0, 500);				// define a 2D orthographic projection matrix
 	// void gluOrtho2D(double left,  double right,  double bottom,  double top);
 	// specify the clipping planes for left, right, bottom and top, in that order <>v^
 	// same as window size
